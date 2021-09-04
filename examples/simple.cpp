0c700c7c05e1305517ca3b0eb2a27354b7d5f7d1
luis <lahoising78@gmail.com>
2021-09-04 00:00:05 -0400

    provide examples in the examples folder

---
+++ examples/simple.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include <ezn_winman.h>
+#include <ezn_window.h>
+
+int main(int argc, char *argv[])
+{
+    ezn::Winman winman;
+    {
+        std::shared_ptr<ezn::Window> window = std::make_shared<ezn::Window>(&winman);
+
+        window->onUpdate = [](ezn::Window *window)
+        {
+            ezn::Input &input = window->GetInput();
+            if(input.IsKeyPressed(ezn::KeyCode::KEY_ESC))
+            {
+                window->Close();
+                return;
+            }
+
+            if(input.IsKeyPressed(ezn::KEY_K))
+            {
+                printf("k is being pressed\n");
+            }
+
+            if(input.KeyJustPressed(ezn::KeyCode::KEY_J))
+            {
+                printf("j was just pressed\n");
+            }
+
+            if(input.KeyReleased(ezn::KeyCode::KEY_L))
+            {
+                printf("L was just released\n");
+            }
+        };
+
+        while(winman.Running())
+        {
+            winman.Update();
+        }
+    }
+    winman.Close();
+
+    return EXIT_SUCCESS;
+}
